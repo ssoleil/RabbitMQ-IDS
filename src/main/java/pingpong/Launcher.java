f0a06ac7367c326da38ad18d5e270573d8b7290c
ssoleil <polina.rohoza@gmail.com>
2023-03-15 22:34:38 +0200

    ping-pong: tested the right version

--- src/main/java/pingpong/Launcher.java
+++ src/main/java/pingpong/Launcher.java
@@ -9,7 +9,30 @@ public class Launcher {
         Node node1 = new Node(1);
         Node node2 = new Node(2);
 
-        node1.start(node2);
+        //without this set up nodes will not know
+        //its partner to send a msg
+        //this set up is NOT required for start() method
+        node1.setPartner(String.valueOf(node2.getId()));
+        node2.setPartner(String.valueOf(node1.getId()));
+
+        //try to run each separate line, and
+        //it will give the @Request failed@ output
+//        node1.sendMsg(new MessageObj(node1.getId(), PING));
+//        node1.sendMsg(new MessageObj(node1.getId(), PONG));
+//
+//        node2.sendMsg(new MessageObj(node1.getId(), PING));
+//        node2.sendMsg(new MessageObj(node1.getId(), PONG));
+
+        //working ping-pong version
+        //node1.start(node2);
+
+        //test if the second START from both nodes is impossible
+//        node1.start(node2);
+//        node2.start(node1);
+
+        //we cant start ping-pong from the bigger node
+        //according to the rules
+        //node2.start(node1);
 
     }
 }
